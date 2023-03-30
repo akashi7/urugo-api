0c73afecab40e127998959b34c8af6cdf6e222be
akashi7 <christiannseko@gmail.com>
2023-03-30 15:44:16 +0200

    project setup

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('urugo app')
+    .setDescription('burugo-api-documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/doc', app, document, {
+    customSiteTitle: 'urugo app',
+    customCss: '.swagger-ui .topbar { display: none }',
+    swaggerOptions: {
+      persistAuthorization: true,
+      docExpansion: 'none',
+    },
+  });
+  await app.listen(process.env.PORT || 5000);
+}
+bootstrap();
